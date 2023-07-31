f1556066c1fae63aaf7c043cee0cd34fd6c7ff19
Amaan Ali <amaanali2002@hotmail.com>
2023-07-30 23:34:42 -0400

    updated font to be inter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300,400,500,600,700,800&display=swap" rel="stylesheet" />
+          <title>IslamGPT</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
