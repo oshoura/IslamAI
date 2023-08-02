218e4e030d475ffd26a06406241d91ad0273b069
oshoura <omarshoura1@gmail.com>
2023-08-02 17:21:05 -0400

    add head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/base.css';
 import type { AppProps } from 'next/app';
 import { Poppins } from 'next/font/google';
-import Head from 'next/head'
+// import Head from 'next/head'
 
 const font = Poppins({
     weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>IslamAI</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
