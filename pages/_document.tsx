219cc26b2b7975cfa693bee59ede1ee175d10718
oshoura <omarshoura1@gmail.com>
2023-08-03 16:55:32 -0400

    add hotjar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Hotjar from '@hotjar/browser';
+
+const siteId = 3600422;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 export default function Document() {
   return (
