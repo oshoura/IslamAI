1fd8055493c7818c3eaca2bbad10b53ca4602ce7
oshoura <omarshoura1@gmail.com>
2023-08-02 20:53:43 -0400

    change context to source documents

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -20,9 +20,9 @@ Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT =
-  PromptTemplate.fromTemplate(`You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+  PromptTemplate.fromTemplate(`You are a helpful AI assistant. Use the following pieces of source documents to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the sources given to you.
+If the question is not related to the source documents, politely respond that you are tuned to only answer questions that are related to the sources given to you.
 
 {context}
 
