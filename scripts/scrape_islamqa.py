47cb0ccb928c39ecc182ca5e880350d637fabfa6
oshoura <omarshoura1@gmail.com>
2023-11-14 13:37:46 -0500

    handle bad links

--- scripts/scrape_islamqa.py
+++ scripts/scrape_islamqa.py
@@ -6,16 +6,34 @@ import aiohttp
 import asyncio
 
 async def fetch(session, url):
-    async with session.get(url) as response:
-        resp = await response.text(), url
-        return resp
+    try:
+        async with session.get(url) as response:
+            # Check if the response status code is 200 (OK)
+            if response.status == 200:
+                resp = await response.text(), url
+            else:
+                print(f"Error {response.status} for URL: {url}")
+                resp = None, None
+    except aiohttp.ClientError as e:
+        print(f"Client error {e} for URL: {url}")
+        resp = None, None
+    except asyncio.TimeoutError:
+        print(f"Timeout error for URL: {url}")
+        resp = None, None
+    except Exception as e:
+        print(f"Failed to get response for URL: {url} due to {e}")
+        resp = None, None
+    return resp
+
 
 async def main(urls):
     async with aiohttp.ClientSession() as session:
         tasks = [fetch(session, url) for url in urls]
-        completed_tasks=  await asyncio.gather(*tasks)
+        completed_tasks = await asyncio.gather(*tasks)
         ret = []
         for html, url in completed_tasks:
+            if (not html):
+                continue
             resp = parse_qa(html, url)
             if resp:
                 ret.append(resp)
@@ -58,7 +76,7 @@ async def run():
             urls = [f'https://islamqa.info/en/answers/{i}' for i in range(last_num, i)]
             last_num = i
             responses = await run_async_tasks(urls)
-            print(f"Got {len(responses)} responses from batch")
+            print(f"Got {len(responses)} responses from batch and {len(islam_qa)} responses in total")
             islam_qa.extend(responses)
         if len(islam_qa) >= 3000:
             with open(f"./islam_qa_part{part}.json", 'w') as file:
